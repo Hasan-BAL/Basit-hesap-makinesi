6d2e8c733cdc5645f0861fc81ddb35ecba4c2e7f
aliemirsimsek <aliemir3425simsek@gmail.com>
2025-10-21 20:53:49 +0300

    carpma islemi
    Eksikliği bulunan çarpma işlemini ekledim

---
+++ carpma.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double sayi1, sayi2, sonuc;
+
+    cout << "=== Carpma Islemi ===" << endl;
+    cout << "Birinci sayiyi girin: ";
+    cin >> sayi1;
+
+    cout << "Ikinci sayiyi girin: ";
+    cin >> sayi2;
+
+    sonuc = sayi1 * sayi2;
+
+    cout << "Sonuc: " << sonuc << endl;
+
+    return 0;
+}
